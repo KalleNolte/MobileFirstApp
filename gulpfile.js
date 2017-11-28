0eee0f768bc5b5917c0a105de445a6ac8f7bb84c
Karl Philipp <karlphilippnolte@yahoo.de>
2017-11-28 18:42:51 +0100

    added gulp automation tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,41 @@
 var gulp = require("gulp"),
-    watch = require("gulp-watch")
+    watch = require("gulp-watch"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssvars = require("postcss-simple-vars"),
+    nested = require("postcss-nested"),
+    cssImport = require("postcss-import"),
+    browserSync = require("browser-sync").create()
 
 gulp.task("default", () => {
     console.log("Default gulp task")
 })
 
-gulp.task("html", () => {
-    console.log("Something to do with html")
-})
 
 gulp.task("styles", () => {
-    console.log("Something to do with css")
+    return gulp.src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"))
+})
+
+gulp.task("cssInject", ["styles"], () => {
+    return gulp.src("./app/temp/styles/styles.css")
+        .pipe(browserSync.stream())
 })
 
 gulp.task("watch", () => {
+   
+    browserSync.init({
+/*         notify: false,
+ */        server: {
+            baseDir: "app"
+        }
+    })
+   
     watch("./app/index.html", () => {
-        gulp.start("html")
+        browserSync.reload()
     })
     watch("./app/assets/styles/**/*.css", () => {
-        gulp.start("styles")
+        gulp.start("cssInject")
     })
 })
