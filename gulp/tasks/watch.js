f09e55d773630f05c37f9959bd97a7e4484979e4
Karl Philipp <karlphilippnolte@yahoo.de>
2017-12-01 03:14:51 +0100

    completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,22 +5,29 @@ var gulp = require("gulp"),
     gulp.task("watch", () => {
 
         browserSync.init({
- /*         notify: false,
-  */        server: {
+         notify: false,
+         server: {
                 baseDir: "app"
             }
-        })
+        });
 
         watch("./app/index.html", () => {
-            browserSync.reload()
-        })
+            browserSync.reload();
+        });
         watch("./app/assets/styles/**/*.css", () => {
-            gulp.start("cssInject")
+            gulp.start("cssInject");
+        });
+        watch("./app/assets/scripts/**/*.js", () => {
+            gulp.start("scriptsRefresh");
         })
-    })
+    });
 
     
 gulp.task("cssInject", ["styles"], () => {
     return gulp.src("./app/temp/styles/styles.css")
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
+});
+
+gulp.task("scriptsRefresh", ["scripts"], function(){
+    browserSync.reload();
 })
