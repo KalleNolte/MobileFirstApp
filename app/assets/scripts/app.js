9a94920e834b8fdfbd5a258f02806860117ed273
Karl Philipp <karlphilippnolte@yahoo.de>
2017-12-02 01:46:43 +0100

    completed the mdoal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 new MobileMenu;
 new RevealOnScroll($(".feature-item"), "80%");
 new RevealOnScroll($(".testimonial"), "100%");
 new StickyHeader;
+new Modal;
